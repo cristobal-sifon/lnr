dea3cdba25bcf2e28bea062c7bbf59bf6cae2aa2
Cristobal Sifon <sifon@astro.princeton.edu>
2017-11-16 15:23:12 -0500

    new structure

--- setup.py
+++ setup.py
@@ -1,9 +1,47 @@
+from __future__ import absolute_import, division, print_function
+
 from setuptools import setup
 
-setup(name='lnr',
-      version='0.1.0',
-      description='Various linear regression modules',
-      author='Cristobal Sifon',
-      author_email='sifon@strw.leidenuniv.nl',
-      url='http://home.strw.leidenuniv.nl/~sifon/lnr/',
-      modules=['lnr'])
+import os
+import re
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+# folder where pygmos is stored
+here = os.path.abspath(os.path.dirname(__file__))
+
+#this function copied from pip's setup.py
+#https://github.com/pypa/pip/blob/1.5.6/setup.py
+#so that the version is only set in the __init__.py and then read here
+#to be consistent
+def find_version(fname):
+    version_file = read(fname)
+    version_match = re.search(
+        r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+#Taken from the Python docs:
+#Utility function to read the README file.
+#Used for the long_description.  It's nice, because now 1) we have a
+#top level README file and 2) it's easier to type in the README file
+#than to put a raw string in below
+def read(fname):
+    return open(os.path.join(here, fname)).read()
+
+
+setup(
+    name='lnr',
+    version='0.1.0',
+    description='Various linear regression modules',
+    long_description=read('README.md'),
+    author='Cristobal Sifon',
+    author_email='sifon@astro.princeton.edu',
+    url='https://github.com/cristobal-sifon/lnr',
+    packages=['lnr'],
+    zip_safe=False
+    )
