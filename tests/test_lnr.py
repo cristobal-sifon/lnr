5d22078595c41e3092c3e14ed768a33033a7ed2d
Cristobal Sifon <cristobal.sifon@pucv.cl>
2019-11-30 10:57:35 -0300

    first mle tests

--- tests/test_lnr.py
+++ tests/test_lnr.py
@@ -9,6 +9,16 @@ logx = np.log10(x)
 xerr = 193
 
 
+def test_mle():
+    testing.assert_raises(AssertionError, mle(1, [1]))
+    testing.assert_raises(AssertionError, mle([1], 1))
+    y = np.array([[1.12005631, 0.3432695],
+                  [0.41998466, 0.2197175],
+                  [0.20493693, 0.09509495]])
+    testing.assert_allclose(
+        mle([0,1,2,3], [1.3,1.2,2.1,2.4], logify=False), y)
+
+
 def test_to_linear():
     # no errors
     testing.assert_allclose(to_linear(logx)[0], x)
